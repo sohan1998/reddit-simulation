d7c03e52f53a5cd7d1085e8314383f478e440ab0
Sohan Shirodkar <sohan1998@gmail.com>
2022-01-05 14:19:38 +0530

    Handle Topic Request

--- kafka-server/server.js
+++ kafka-server/server.js
@@ -37,3 +37,33 @@ const getinvitations = require('./services/GetInvitations');
 const maxpostcommunity = require('./services/MaxPostCommunity');
 const maxpostuser = require('./services/MaxPostUser');
 const addpostimage = require('./services/AddPostImage');
+
+function handleTopicRequest(topic_name, fname) {
+    //var topic_name = 'root_topic';
+    var consumer = connection.getConsumer(topic_name);
+    var producer = connection.getProducer();
+    console.log('server is running');
+    consumer.on('message', function (message) {
+        console.log('message received for ' + topic_name + ' ', fname);
+        console.log(JSON.stringify(message.value));
+        var data = JSON.parse(message.value);
+
+        fname.handle_request(data.data, function (err, res) {
+            console.log('after handle' + res);
+            var payloads = [
+                {
+                    topic: data.replyTo,
+                    messages: JSON.stringify({
+                        correlationId: data.correlationId,
+                        data: res,
+                    }),
+                    partition: 0,
+                },
+            ];
+            producer.send(payloads, function (err, data) {
+                console.log(data);
+            });
+            return;
+        });
+    });
+}
